af11197beae8e275519bca3df2901aea0c646baa
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-05-16 13:33:42 +0200

    Configure store and combine reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import { applyMiddleware, combineReducers, legacy_createStore as createStore } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import rocketReducer from './rockets/rockets';
 
-const configureStore = () => <div>configureStore</div>;
+const rootReducer = combineReducers({ rocketReducer });
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
-export default configureStore;
+export default store;
