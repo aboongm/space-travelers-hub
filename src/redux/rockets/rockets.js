96e0d4e764925d0e4780b11528e0b7463f4e1fdf
aboongm <aboongm@yahoo.com>
2022-05-16 18:32:57 +0530

    Create action creator and rocket reducer and update rocketReducer in store

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,7 +1,59 @@
-import React from 'react';
+import axios from 'axios';
 
-export default function rockets() {
-  return (
-    <div>rockets</div>
-  );
-}
+// constants
+const RESERVE_ROCKET = 'spaceTravelerHub/rockets/RESERVE_ROCKET';
+const FETCH_ROCKET = 'spaceTravelerHub/rockets/FETCH_ROCKET';
+const url = 'https://api.spacexdata.com/v3/rockets';
+const initialState = [];
+
+// action creators
+export const reserveRocketAction = (payload) => ({
+  type: RESERVE_ROCKET,
+  payload,
+});
+
+const fetchRocketAction = (payload) => ({
+  type: FETCH_ROCKET,
+  payload,
+});
+
+export const fetchRocketApiAction = () => async (dispatch) => {
+  const rockets = await axios.get(url);
+  const rocketFetch = Object.entries(rockets.data).map((rocket) => {
+    const images = rocket[1].flickr_images[0];
+    const { id, rocket_name: name, description, reserved = false } = rocket[1];
+    return {
+      id,
+      images,
+      name,
+      description,
+      reserved,
+    };
+  });
+
+  dispatch(fetchRocketAction(rocketFetch));
+};
+
+const bookRocket = (state, payload) => {
+  const nextState = state.map((rocket) => {
+    console.log('rocket id: ', rocket.id);
+    console.log('payload: ', payload);
+    if (rocket.id !== payload) return rocket;
+    return { ...rocket, reserved: !rocket.reserved };
+  });
+  return nextState;
+};
+
+// reducer
+const rocketReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case RESERVE_ROCKET:
+      return bookRocket(state, action.payload);
+    case FETCH_ROCKET:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default rocketReducer;
