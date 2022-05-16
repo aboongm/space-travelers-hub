eb058898dda71130b809b73c10d0676de6e8d7ae
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-05-16 13:54:48 +0200

    Fetch data and setup reducers

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,7 +1,59 @@
-import React from 'react';
+import axios from 'axios';
 
-export default function missions() {
-  return (
-    <div>missions</div>
-  );
-}
+// constants
+const RESERVE_MISSION = 'spaceTravelerHub/rockets/RESERVE_MISSION';
+const FETCH_MISSION = 'spaceTravelerHub/rockets/FETCH_MISSION';
+const url = 'https://api.spacexdata.com/v3/missions';
+const initialState = [];
+
+// action creators
+export const reserveMissionAction = (payload) => ({
+  type: RESERVE_MISSION,
+  payload,
+});
+
+const fetchMissionAction = (payload) => ({
+  type: FETCH_MISSION,
+  payload,
+});
+
+export const fetchMissionApiAction = () => async (dispatch) => {
+  const missions = await axios.get(url);
+  const missionFetch = Object.entries(missions.data).map((mission) => {
+    const {
+      mission_id: id, mission_name: name, description, reserved = false,
+    } = mission[1];
+    return {
+      id,
+      name,
+      description,
+      reserved,
+    };
+  });
+
+  dispatch(fetchMissionAction(missionFetch));
+};
+
+const bookMission = (state, payload) => {
+  const nextState = state.map((mission) => {
+    console.log('mission id: ', mission.id);
+    console.log('payload: ', payload);
+    if (mission.id !== payload) return mission;
+    return { ...mission, reserved: !mission.reserved };
+  });
+  return nextState;
+};
+
+// reducer
+const missionReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case RESERVE_MISSION:
+      return bookMission(state, action.payload);
+    case FETCH_MISSION:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default missionReducer;
--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,68 +1,7 @@
-import axios from 'axios';
+import React from 'react';
 
-// constants
-const RESERVE_ROCKET = 'spaceTravelerHub/rockets/RESERVE_ROCKET';
-// const CANCEL_RESERVATION = 'spaceTravelerHub/rockets/CANCEL_RESERVATION';
-const FETCH_ROCKET = 'spaceTravelerHub/rockets/FETCH_ROCKET';
-const url = 'https://api.spacexdata.com/v3/rockets';
-const initialState = [];
-
-// action creators
-export const reserveRocketAction = (payload) => ({
-  type: RESERVE_ROCKET,
-  payload,
-});
-
-// export const cancelReservationAction = (payload) => ({
-//   type: CANCEL_RESERVATION,
-//   payload,
-// });
-
-const fetchRocketAction = (payload) => ({
-  type: FETCH_ROCKET,
-  payload,
-});
-
-export const fetchRocketApiAction = () => async (dispatch) => {
-  const rockets = await axios.get(url);
-  const rocketFetch = Object.entries(rockets.data).map((rocket) => {
-    const images = rocket[1].flickr_images[0];
-    const {
-      id, rocket_name: name, description, reserved = false,
-    } = rocket[1];
-    return {
-      id,
-      images,
-      name,
-      description,
-      reserved,
-    };
-  });
-  // console.log(rocketFetch);
-
-  dispatch(fetchRocketAction(rocketFetch));
-};
-
-const bookRocket = (state, payload) => {
-  const nextState = state.map((rocket) => {
-    console.log('rocket id: ', rocket.id);
-    console.log('payload: ', payload);
-    if (rocket.id !== payload) return rocket;
-    return { ...rocket, reserved: !rocket.reserved };
-  });
-  return nextState;
-};
-
-// reducer
-const rocketReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case RESERVE_ROCKET:
-      return bookRocket(state, action.payload);
-    case FETCH_ROCKET:
-      return action.payload;
-    default:
-      return state;
-  }
-};
-
-export default rocketReducer;
+export default function rockets() {
+  return (
+    <div>rockets</div>
+  );
+}
