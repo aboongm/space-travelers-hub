db3b196da8fd9c6de702b9984939e597754254fc
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-05-16 13:10:43 +0200

    Create Redux state slices and modify components

---
+++ src/redux/missions/missions.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function missions() {
+  return (
+    <div>missions</div>
+  );
+}
---
+++ src/redux/rockets/rockets.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function rockets() {
+  return (
+    <div>rockets</div>
+  );
+}
