9d3b61d0aa720907e8e811b0dceb3949bf052016
aboongm <aboongm@yahoo.com>
2022-05-17 15:06:02 +0530

    Update My Profile page to display reserved rockets

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,69 +1,56 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getMissions } from '../redux/missions/missions';
-// import { fetchRocketApiAction } from '../redux/rockets/rockets';
+import { fetchRocketApiAction } from '../redux/rockets/rockets';
 
 const MyProfile = () => {
   const dispatch = useDispatch();
   const missions = useSelector((state) => state.missions);
-  // const rockets = useSelector((state) => state.rockets);
+  const rockets = useSelector((state) => state.rocketReducer);
 
   useEffect(() => {
     if (missions.length === 0) {
       dispatch(getMissions());
     }
-    // if (rockets.length === 0) {
-    //   dispatch(fetchRocketApiAction);
-    // }
+    if (rockets.length === 0) {
+      dispatch(fetchRocketApiAction);
+    }
   }, []);
 
-  return (
+  const rocketList = useSelector((state) => state.rocketReducer);
+  const myRocket = rocketList.filter((rocket) => rocket.reserved);
+  console.log('profile', myRocket);
 
+  return (
     <div className="container">
-      <div className="row align-items-end">
+      <div className="row mt-4">
         <div className="col">
           <h2 className="text-center">My Missions</h2>
           <ul>
-            {missions.filter((msn) => msn.reserved === true).map((fmsn) => (
-              <li
-                key={fmsn.id}
-                className="border p-3"
-              >
-                {fmsn.name}
-              </li>
-            ))}
+            {missions
+              .filter((msn) => msn.reserved === true)
+              .map((fmsn) => (
+                <li key={fmsn.id} className="border p-3">
+                  {fmsn.name}
+                </li>
+              ))}
           </ul>
         </div>
 
         <div className="col">
-          <h2 className="text-center">My Rockets</h2>
-          <ul>
-            {missions.filter((msn) => msn.reserved === true).map((fmsn) => (
-              <li
-                key={fmsn.id}
-                className="border p-3"
-              >
-                {fmsn.name}
-              </li>
-            ))}
-          </ul>
+          <h3 className="mb-3">My Rockets</h3>
+          {myRocket.length ? (
+            myRocket.map((rocket) => (
+              <p className="border m-0 p-3" key={rocket.id}>
+                {rocket.name}
+              </p>
+            ))
+          ) : (
+            <p>No rocket to display</p>
+          )}
         </div>
       </div>
     </div>
-  /*      <div className="w-1/2 flex flex-col gap-3">
-        <h2 className="font-bold text-xl">Rockets</h2>
-        <ul className="border">
-          {rockets.filter((rocket) => rocket.reserved === true).map((rocket) => (
-            <li
-              key={rocket.id}
-              className="border p-3"
-            >
-              {rocket.name}
-            </li>
-          ))}
-        </ul>
-      </div>  */
-
   );
 };
 
